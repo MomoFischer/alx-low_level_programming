44800cde16cc7b9f55d971712396847c76f74089
MomoFischer <muhammad.elsayad22.com>
2023-06-15 18:08:40 -0700

    Hello C world

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,12 @@
-#include<unistd.h>
+#include <unistd.h>
 
-/***/
+/**
  * main - Entry point
- * Return: Always 1 (Success)
+ *
+ * Description: print
  */
+
 int main(void)
-{
-	write(STDOUT_FILENO, "and that piece of art is useful
-			\" - Dora Korpar, 2015-10-19\n", 59);
-	return (1);
-}
+	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	write(1, quo ,59)
+	return(1);
