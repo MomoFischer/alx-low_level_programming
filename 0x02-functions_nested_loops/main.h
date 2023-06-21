fcfb9ef79ec893c2d10554b7807bbb2d198036e6
MomoFischer <muhammad.elsayad22.com>
2023-06-20 19:09:30 -0700

    work on2

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,11 +1,5 @@
-<<<<<<< HEAD
 #ifndef MAIN_HEADER_FILE
 #define MAIN_HEADER_FILE
-=======
-#ifndef MAIN_H
-#define MAIN_H
-#include <stdio.h>
->>>>>>> a15d150f87c4eed0e1f7c426ab0ab87a37637133
 
 int _putchar(char c);
 void print_alphabet(void);
@@ -19,10 +13,5 @@ void jack_bauer(void);
 void times_table(void);
 int add(int, int);
 void print_to_98(int n);
-<<<<<<< HEAD
-
-=======
-void print_times_table(int n);
 
 #endif
->>>>>>> a15d150f87c4eed0e1f7c426ab0ab87a37637133
