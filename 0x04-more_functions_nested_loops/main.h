9b9a65b6d5b1e797a33c88512115978146fc522b
MomoFischer <muhammad.elsayad22.com>
2023-06-22 17:55:29 -0700

    work on 0

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,7 +1,10 @@
 #ifndef MAIN
 #define MAIN
+
+#include <stdio.h>
+
 extern int _putchar(char c);
-extern int _int _isupper(int c);;
+extern int _int _isupper(int c);
 extern int _isdigit(int c);
 extern int mul(int a, int b);
 extern void print_numbers(void);
